732f1ee6ed68b27131cc18082526f2681bbc7a61
Stian Mogen <stianfmo@stud.ntnu.no>
2023-02-13 14:00:12 +0100

    data

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
-api_url = "https://rest.uniprot.org/uniprotkb/search?query=(reviewed:true)%20AND%20(organism_id:9606)"
+api_url = "https://rest.uniprot.org/uniprotkb/search?query=(reviewed:true)%20AND%20(organism_id:9940)"
+api_url2 = "https://rest.uniprot.org/uniprotkb/search?query=(reviewed:true)%20AND%20(gene_exact:APOH_HUMAN)"
+api_url3 = "https://rest.uniprot.org/uniprotkb/search?query=gene_exact:APOH&format=json"
 response = requests.get(api_url)
 response.json()
 
+#350	APOH 	apolipoprotein H (beta-2-glycoprotein I)
 def print_response():
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {response.text}')  # Press ⌘F8 to toggle the breakpoint.
