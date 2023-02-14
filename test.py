aa311b8ab31802169a9a3014c4586c54e961da3e
Stian Mogen <stianfmo@stud.ntnu.no>
2023-02-14 08:44:46 +0100

    load model with custom objects

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 from tensorflow import keras
-model = keras.models.load_model('./model/')
 
-ans = model.predict("EEDPDLKAAIQESLREAEEA")
+
+def f1(y_true, y_pred):
+    return 1
+
+model = keras.models.load_model('./model/', custom_objects={'f1':f1}, compile=False)
+model.compile(metrics=["accuracy", f1])
+ans = model.predict(["EEDPDLKAAIQESLREAEEA"])
 print(ans)
 
 
